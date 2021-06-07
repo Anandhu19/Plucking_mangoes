ab15d629e18d802c1f9f62abfe4417f772ae7bdf
Anandhu19 <84615582+Anandhu19@users.noreply.github.com>
2021-06-07 22:08:00 +0530

    Add files via upload

---
+++ sling.js
@@ -0,0 +1,51 @@
+class sling{
+
+    constructor(bodyA,pointB){
+  
+        var options ={
+            bodyA :bodyA,
+           pointB : pointB,
+            stiffness : 0.04,
+            length : 10
+        }
+    this.pointB = pointB
+
+        this.sling=Constraint.create(options);
+        World.add(world,this.sling);
+
+    }
+
+display(){
+ 
+if(this.sling.bodyA){
+   var pos1 = this.sling.bodyA.position
+   var pos2 = this.pointB
+ strokeWeight(3);
+    line(pos1.x,pos1.y,pos2.x,pos2.y);
+}
+}
+
+
+fly(){
+
+    this.sling.bodyA = null
+}
+
+attach(bodyA){
+   
+    this.sling.bodyA = bodyA
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
